7299da10c2d30d145bcfb7cac777479d8c709039
한광재 <hkj4900@pusan.ac.kr>
2022-03-03 19:40:50 +0900

    Test: ContetnList 관련 TC 추가

---
+++ test/unit/Content/ContentList.spec.js
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import ContentList from '@/components/ContentList.vue'
+import Content from '@/components/Content.vue'
+
+describe('ContentList.vue', () => {
+  test('ContentList 렌더링', () => {
+    const wrapper = mount(ContentList);
+
+    expect(wrapper.find('[data-test="ContentList"]').exists()).toBeTruthy();
+  })
+
+  test('N개의 Content가 있을 때 N개를 렌더링하는지', () => {
+    const arrSize = 4;
+    const contents = Array(arrSize).fill({
+      id: 1,
+      img: 'test',
+      title: 'npm은 무엇일까',
+      description: 'ttt',
+      author: 'tester',
+      like: 17
+    });
+    const wrapper = mount(ContentList, {
+      propsData: {
+        contents
+      }
+    });
+
+    expect(wrapper.findAllComponents(Content)).toHaveLength(arrSize);
+  })
+})
