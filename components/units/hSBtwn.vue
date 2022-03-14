a31f55ca93a8aeca948e9311889f7ed61b911dd8
한광재 <hkj4900@pusan.ac.kr>
2022-03-14 20:08:15 +0900

    Feat: 개발에 자주 사용되는 유닛 컴포넌트 구현

---
+++ components/units/hSBtwn.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hsbtwn" :style="{width, height}">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '100%'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hsbtwn {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
---
+++ components/units/vSBtwn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="vsbtwn" :style="{width, height}">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '100%'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.vsbtwn {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+}
+</style>
