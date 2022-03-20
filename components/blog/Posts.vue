51967ebe1aacbcea9d2eb4039e921e6d4e27e1e2
한광재 <hkj4900@pusan.ac.kr>
2022-03-20 15:17:16 +0900

    Feat: 특정 블로그의 Post 리스트 컴포넌트 구현

---
+++ components/blog/Posts.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="posts-wrapper">
+    <p class="noti">총 <span style="color: #2BDD5D">{{posts.length}}</span>개의 포스팅이 존재합니다</p>
+    <post
+      v-for="post in posts"
+      :key="post.id"
+      v-bind="post"
+    />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+export default {
+  name: 'BlogPosts',
+  components: { Post },
+  props: {
+    posts: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.posts-wrapper {
+  padding: 29px 58px 0px 71px;
+}
+
+.noti {
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 29px;
+  margin: 0px;
+  padding-bottom: 10px;
+
+  border-bottom: 1px solid #535353;
+}
+</style>
