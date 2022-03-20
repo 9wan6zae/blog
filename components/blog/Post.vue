cd5b16551bb5382a1e1376da59886e1481c176dc
한광재 <hkj4900@pusan.ac.kr>
2022-03-20 15:16:07 +0900

    Feat: Post 구현

---
+++ components/blog/Post.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="post-wrapper">
+    <h-stack height="140px" justify-content="space-between">
+      <v-stack justify-content="space-between">
+        <div>
+          <p class="title">{{title}}</p>
+          <p class="description">{{description}}</p>
+        </div>
+        <h-stack class="post-info" height="20px" align-items="center">
+          <p class="category">{{category}}</p>
+          <div class="divider" />
+          <p class="createdAt">{{createdAt}}</p>
+        </h-stack>
+      </v-stack>
+      <img class="thumbnail" :src="thumbnail" />
+    </h-stack>
+  </div>
+</template>
+
+<script>
+import HStack from '../unit/hStack.vue'
+import VStack from '../unit/vStack.vue'
+
+export default {
+  name: 'BlogPost',
+  components: { VStack, HStack },
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description:  {
+      type: String,
+      default: ''
+    },
+    thumbnail:  {
+      type: String,
+      default: ''
+    },
+    category:  {
+      type: String,
+      default: ''
+    },
+    author:  {
+      type: String,
+      default: ''
+    },
+    createdAt:  {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-wrapper {
+  width: 100%;
+  padding: 40px 0px;
+  border-bottom: 1px solid #2f3033;
+}
+
+.title {
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 35px;
+  margin: 0;
+}
+
+.description {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -0.03em;
+
+  color: #D2D3D3;
+}
+
+.thumbnail {
+  width: 140px;
+  height: 140px;
+  object-fit: cover;
+}
+
+.post-info {
+  margin: 0;
+}
+
+.post-info > p {
+  margin: 0 10px
+}
+
+.post-info > p:first-child {
+  margin-left: 0px
+}
+
+.post-info > p:last-child {
+  margin-right: 0px
+}
+
+.category {
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -0.03em;
+
+  color: #2BDD5D;
+}
+
+.createdAt {
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -0.03em;
+
+  color: #A6A6A7;
+}
+
+.divider {
+  width: 1px;
+height: 15px;
+left: 670px;
+top: 664px;
+
+background: #36373C;
+}
+</style>
