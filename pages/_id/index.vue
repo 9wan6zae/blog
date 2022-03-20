5bbbee4cc2b24e6f2f4ad538e77d248f961734cd
한광재 <hkj4900@pusan.ac.kr>
2022-03-20 15:17:39 +0900

    Feat: 특정 블로그 화면 구현

---
+++ pages/_id/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <p>{{id}}</p>
+    <div id="wrapper">
+      <main>
+        <profile
+          v-bind="profile"
+        />
+        <posts :posts="posts" />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Posts from '../../components/blog/Posts.vue'
+import Profile from '../../components/blog/Profile.vue'
+export default {
+  name: 'CustomerBlog',
+  components: {
+    Profile,
+    Posts
+  },
+  asyncData({params}) {
+    return {
+      id: params.id
+    }
+  },
+  data() {
+    return {
+      id: '',
+      profile: {
+        nickname: 'tester',
+        profileImg: 'http://blog.jinbo.net/attach/615/200937431.jpg',
+        introduce: 'testtest',
+        githubUrl: '',
+      },
+      posts: [
+        {
+          id: 0,
+          title: 'test',
+          description: 'test',
+          thumbnail: 'http://blog.jinbo.net/attach/615/200937431.jpg',
+          category: 'test',
+          author: 'test',
+          createdAt: '2022-01-01'
+        },
+        {
+          id: 1,
+          title: 'test',
+          description: 'test',
+          thumbnail: 'http://blog.jinbo.net/attach/615/200937431.jpg',
+          category: 'test',
+          author: 'test',
+          createdAt: '2022-01-01'
+        },
+        {
+          id: 2,
+          title: 'test',
+          description: 'test',
+          thumbnail: 'http://blog.jinbo.net/attach/615/200937431.jpg',
+          category: 'test',
+          author: 'test',
+          createdAt: '2022-01-01'
+        },
+        {
+          id: 4,
+          title: 'test',
+          description: 'test',
+          thumbnail: 'http://blog.jinbo.net/attach/615/200937431.jpg',
+          category: 'test',
+          author: 'test',
+          createdAt: '2022-01-01'
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+#wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+main {
+  width: 849px;
+}
+</style>
