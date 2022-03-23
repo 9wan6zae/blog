26ff7a355278bb0b3d039c517360ebf5c548c2d8
한광재 <hkj4900@pusan.ac.kr>
2022-03-23 12:36:46 +0900

    Feat: 기본 레이아웃 설정

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <app-header />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader.vue'
+export default {
+  name: 'DefaultLayout',
+  components: { AppHeader },
+}
+</script>
