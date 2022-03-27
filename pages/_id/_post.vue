1185ede1ce1d0e20dc336d00c98ee16918b89639
한광재 <hkj4900@pusan.ac.kr>
2022-03-27 22:51:18 +0900

    Feat: PostPage 구현

--- pages/_id/_post.vue
+++ pages/_id/_post.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>
-    <p>{{ post }}</p>
+  <div id="post-wrapper">
+    <h1 id="title" data-test="title">{{title}}</h1>
+    <h-stack justify-content="space-between">
+      <h-stack division>
+        <p id="category" data-test="category">node</p>
+        <p id="createdAt" data-test="createdAt">2022.01.01</p>
+      </h-stack>
+      <h-stack id="modify-btn" division width="10vw">
+        <p data-test="edit-btn">수정</p>
+        <p data-test="delete-btn">삭제</p>
+      </h-stack>
+    </h-stack>
+    <!-- eslint-disable vue/no-v-html -->
+    <div id="viewer" data-test="viewer" v-html="compiledMarkdown" />
   </div>
 </template>
 
 <script>
+import marked from 'marked'
+import hStack from '../../components/unit/hStack.vue'
+
 export default {
+  components: { hStack },
   asyncData({ params }) {
     return {
       post: params.post,
     }
   },
+  data() {
+    return {
+      title: '타이틀입니다',
+      input: '# test\n## test'
+    }
+  },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.input)
+    },
+  },
 }
 </script>
+
+<style scoped>
+#post-wrapper {
+  width: 100%;
+  max-width: 720px;
+  margin: 0 auto;
+}
+</style>
