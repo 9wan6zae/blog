05f0ae807f81efb6ffa289f7ebb25d4b7fa2e903
GwangJae Han <hkj4900@pusan.ac.kr>
2022-03-29 19:44:52 +0900

    Post 페이지 구현 (#1)
    * Refactor: Post 컴포넌트를 PostBlock 컴포넌트로 이름 변경
    
    * Test: PostPage 테스트 코드 추가
    
    * Feat: PostPage 구현
    
    * Refactor: hStack 요소 사이에 구분자가 생기는 옵션 구현
    
    * Design: 스타일 지정
    
    * Style: Lint 적용
    
    * Feat: 헤딩을 파싱해 앵커 기능 구현
    
    * Desigin: anchor 리스트의 position을 fixed로 변경
    
    * Style: Lint 적용

---
+++ components/post/anchor.vue
@@ -0,0 +1,54 @@
+<template>
+  <div style="position: relative">
+    <article id="anchor-wrapper">
+      <v-stack id="anchor" width="160px">
+        <a
+          v-for="a in anchors"
+          :key="a.id"
+          :href="`#${a.id}`"
+          :style="{ marginLeft: `${(a.level - 1) * 20}px` }"
+        >
+          {{ a.text }}
+        </a>
+      </v-stack>
+    </article>
+  </div>
+</template>
+
+<script>
+import vStack from '../unit/vStack.vue'
+export default {
+  name: 'PostAnchor',
+  components: { vStack },
+  props: {
+    anchors: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+#anchor-wrapper {
+  position: absolute;
+  left: 100%;
+}
+
+#anchor {
+  position: fixed;
+}
+.anchor1 {
+  margin-left: 0;
+}
+
+.anchor2 {
+  margin-left: 20px;
+}
+
+.anchor3 {
+  margin-left: 40px;
+}
+</style>
--- pages/_id/_post.vue
+++ pages/_id/_post.vue
@@ -1,15 +1,105 @@
 <template>
-  <div>
-    <p>{{ post }}</p>
+  <div id="post-wrapper">
+    <h1 id="title" data-test="title">{{ title }}</h1>
+    <anchor :anchors="anchors" />
+    <h-stack id="post-info" justify-content="space-between">
+      <h-stack division>
+        <p id="category" data-test="category">node</p>
+        <p id="createdAt" data-test="createdAt">2022.01.01</p>
+      </h-stack>
+      <h-stack id="modify-btns" division width="10vw">
+        <a data-test="edit-btn">수정</a>
+        <a data-test="delete-btn">삭제</a>
+      </h-stack>
+    </h-stack>
+    <!-- eslint-disable vue/no-v-html -->
+    <div id="viewer" data-test="viewer" v-html="compiledMarkdown" />
+    <div style="height: 100vh" />
   </div>
 </template>
 
 <script>
+import marked from 'marked'
+import hStack from '../../components/unit/hStack.vue'
+import Anchor from '../../components/post/anchor.vue'
+
 export default {
+  components: { hStack, Anchor },
   asyncData({ params }) {
     return {
       post: params.post,
     }
   },
+  data() {
+    return {
+      title: '타이틀입니다',
+      content: `# 가나다 라마\n# test test\n# 가나다 라마\n## test\n## test\n### abc\n### test test\n# abc\n# abc`,
+      markedContent: '',
+      ids: {},
+      anchors: [],
+    }
+  },
+  computed: {
+    compiledMarkdown() {
+      const renderer = new marked.Renderer()
+
+      renderer.heading = (text, level) => {
+        let id = text.replace(' ', '-')
+
+        if (id in this.ids) {
+          this.ids[id].dup += 1
+          id = `${id}-${this.ids[id].dup}`
+        } else {
+          this.ids[id] = {
+            dup: 0,
+          }
+        }
+
+        this.anchors.push({
+          text,
+          id,
+          level,
+        })
+
+        return `
+          <h${level} id="${id}">
+            ${text}
+          </h${level}>
+        `
+      }
+      return marked(this.content, { renderer })
+    },
+  },
 }
 </script>
+
+<style scoped>
+#post-wrapper {
+  width: 100%;
+  max-width: 720px;
+  margin: 0 auto;
+}
+
+#title {
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 58px;
+}
+
+#post-info {
+  font-weight: 400;
+  font-size: 16px;
+}
+
+#category {
+  color: var(--emphasis);
+}
+
+#createdAt {
+  color: #a6a6a7;
+}
+
+#modify-btns > a {
+  color: var(--emphasis);
+}
+</style>
